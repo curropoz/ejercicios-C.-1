bc9b63ccf5e15bf009a0ad577091600064d6fe94
curropoz <fjpozuelosromero@gmail.com>
2016-03-22 00:35:21 +0100

    Added files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+
+printf("ejercicio 1\n");
+/*cometarios*/
+
+printf("Un ¡Hola Mundo! como Dios manda");
+
+printf("para finalizar pulse intro");
+
+/*getchar();
+
+return 0;
+*/
+}
